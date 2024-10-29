7ca4c1a533e468e2a222015354426fa7751d646f
ZuYun <jonas.jzy@gmail.com>
2024-10-29 23:09:37 +0800

    build: 更新 surgery 依赖版本并调整 CI 构建配置
    - 将 surgery-helper 和 surgery-api版本更新至 24.10.29
    - 修改 settings.gradle 中的条件判断，根据环境变量动态调整构建配置
    - 在 buildSrc/build.gradle.kts 中添加 CI 环境判断，以方便调试

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -3,12 +3,14 @@ plugins {
     alias(vcl.plugins.kotlin.jvm)
 }
 
-//这里配置需要加载进buildSrc编译的代码方便调试
-sourceSets.main {
-    java.srcDirs(
-//        """..\surgery-doctor-tryfinally\src\main\java""",
-//        """..\surgery-doctor-arouter\src\main\java"""
-    )
+val isCi = System.getenv("CI") == "true" || System.getenv("GITHUB_ACTIONS") == "true" || System.getenv("JENKINS_HOME") != null
+if (isCi) {//这里配置需要加载进buildSrc编译的代码方便调试
+    sourceSets.main {
+        java.srcDirs(
+            """..\surgery-doctor-tryfinally\src\main\java""",
+            """..\surgery-doctor-arouter\src\main\java"""
+        )
+    }
 }
 
 dependencies {
